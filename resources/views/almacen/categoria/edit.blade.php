192cdd0313356a33606e0ef1440b745eff9d0709
William de León <68469930+Sinder20@users.noreply.github.com>
2021-04-25 15:53:32 -0600

    Actualización Detalles

--- resources/views/almacen/categoria/edit.blade.php
+++ resources/views/almacen/categoria/edit.blade.php
@@ -21,7 +21,7 @@
             </div>
             <div class="form-group">
                 <button class="btn btn-primary" type="submit">Guardar</button>
-                <button class="btn btn-danger" type="submit">Cancelar</button>
+                <button class="btn btn-danger" type="reset">Cancelar</button>
             </div>
             {!! Form::close() !!}
         </div>
--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -27,7 +27,7 @@
         <!-- Logo -->
         <a href="http://127.0.0.1:8000" class="logo">
             <!-- mini logo for sidebar mini 50x50 pixels -->
-            <span class="logo-mini"><b>AD</b>V</span>
+            <span class="logo-mini"><b>S</b>GT</span>
             <!-- logo for regular state and mobile devices -->
             <span class="logo-lg"><b>Sinder Ventas</b></span>
         </a>
